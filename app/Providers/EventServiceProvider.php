82389fdfc81362c7a3fda6ae8c9590904ee4d18f
MohamedAbakrim <mohammedabakrim@gmail.com>
2024-02-15 14:44:29 +0300

    notifications and events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Events\ChirpCreated;
+use App\Listeners\SendChirpCreatedNotifications;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
@@ -17,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        ChirpCreated::class => [
+            SendChirpCreatedNotifications::class,
         ],
     ];
 
